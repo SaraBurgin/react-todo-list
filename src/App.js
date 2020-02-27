bd3a624d33430c2f8a49f17959912ab0dd746c11
Sara <saragonzalezburgin@hotmail.com>
2020-02-27 16:25:19 +0100

    Finish delete todo button, add some css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: #d3d3d3;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
 
   h1 {
+    display: block;
     text-align: center;
     color: #57595d;
     font-weight: bold;
@@ -12,28 +16,25 @@ const Container = styled.div`
     margin-top: -10px;
     margin-bottom: 10px;
     padding-top: 20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-  p {
-    display: flex;
-    margin-left: 400px;
-    align-items: center;
-    font-size: 20px;
-    font-weight: bold;
-    color: #57595d;
-  }
+
   form input {
-    margin-left: 550px;
+    display: inline-block;
     width: 250px;
     padding-left: 30px;
     padding-right: 30px;
     padding-top: 20px;
     padding-bottom: 20px;
-    margin-bottom: 20px;
+    margin-left: 50px;
     text-align: center;
     font-size: 20px;
+    border: 4px solid #40e0d0;
   }
-  button {
-    margin-left: 5px;
+  form > button {
+    display: inline-block;
+    margin-left: 10px;
     padding-left: 10px;
     padding-right: 10px;
     padding-top: 10px;
@@ -42,15 +43,50 @@ const Container = styled.div`
     background-color: #40e0d0;
     color: #ffffff;
   }
-  div button {
+  p {
     display: block;
-    margin-left: 900px;
+    text-decoration: underline;
+    margin-left: -400px;
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold;
+    color: #57595d;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  div > input[type='checkbox'] {
+    display: inline-block;
+    position: relative;
+    width: 20px;
+  }
+
+  div {
+    display: flex;
+    position: relative;
+    width: 500px;
+    color: #ffffff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: bold;
+    font-size: 25px;
+  }
+
+  div > button {
+    display: inline-block;
+    position: relative;
+    border-radius: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #40e0d0;
+    color: #ffffff;
   }
 `;
 
 export default function App() {
   const [todos, setTodos] = useState([]);
-  const [draft, setDraft] = useState({ id: null, name: '', completed: false });
+  const [draft, setDraft] = useState({ id: null, name: '', completed: false }); //draft is to be able to have control over the information that the user will type before submitting.
 
   function handleChange(event) {
     const newDraft = {
@@ -62,34 +98,16 @@ export default function App() {
   }
 
   function handleSubmit(event) {
-    const newTodos = [draft, ...todos];
-    setTodos(newTodos);
+    const newTodos = [draft, ...todos]; //Making a copy//...=it will take everything//
     setDraft({ id: null, name: '', completed: false });
+    setTodos(newTodos);
     event.preventDefault();
   }
 
-  function deleteTodo(todoInList) {
-    const { todos } = this.state;
-    const newTodos = [...todos];
-    const itemIndex = newTodos.findIndex(todo => todo.name === todoInList.name);
-
-    if (itemIndex > -1) {
-      newTodos.splice(itemIndex, 1);
-    } else {
-      newTodos.push(todoInList);
-    }
-
-    this.setState({
-      todos: newTodos,
-    });
+  function deleteTodo(id) {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
   }
-
-  // function deleteTodo(id) {
-  //   const newTodos = [...todos];
-  //   const todo = newTodos.find(todo => todo.id === id);
-  //   newTodos.splice(todo, 1);
-  //   setTodos(newTodos);
-  // }
 
   return (
     <>
@@ -106,9 +124,10 @@ export default function App() {
           <br></br>
           <p>List of todos:</p>
           {todos.map(todoInList => {
+            //map gives you the possibility of changing the object from string to object for example.
             return (
               <div key={todoInList.id}>
-                <button className="x" onClick={deleteTodo}>
+                <button className="x" onClick={() => deleteTodo(todoInList.id)}>
                   x
                 </button>
                 <input
@@ -119,6 +138,7 @@ export default function App() {
                       todos.map(todo => {
                         const newTodo = { ...todo };
                         if (todo.id === todoInList.id) {
+                          //=== : mean equal // !== : the opposite
                           if (todo.completed === true) {
                             newTodo.completed = false;
                           } else {
