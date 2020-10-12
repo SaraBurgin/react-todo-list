d57892d9063b6c803ccf796b29e8a666e6d87a2b
Sara <saragonzalezburgin@hotmail.com>
2020-10-12 14:38:57 +0200

    Fix CSS

--- src/App.js
+++ src/App.js
@@ -3,120 +3,89 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: #d3d3d3;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 100vw;
+  grid-template-rows: 1fr 2fr 1fr 5fr;
+  justify-items: center;
+  height: 100vh;
 
   h1 {
-    display: block;
     text-align: center;
     color: #57595d;
     font-weight: bold;
     font-size: 50px;
-    margin-top: -10px;
-    margin-bottom: 10px;
-    padding-top: 20px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: sans-serif;
   }
 
   form input {
-    display: inline-block;
-    width: 250px;
-    padding-left: 30px;
-    padding-right: 30px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    margin-left: 50px;
+    width: 300px;
+    padding: 20px;
     text-align: center;
+    align-self: start;
     font-size: 20px;
     border: 4px solid #40e0d0;
+    border-radius: 5px;
   }
+
   form > button {
-    display: inline-block;
-    margin-left: 10px;
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 10px;
-    padding-bottom: 10px;
+    padding: 10px;
     border-radius: 10px;
     background-color: #40e0d0;
+    margin-left: 10px;
+    border: 2px solid #57595d;
     color: #ffffff;
   }
-  p {
-    display: block;
+
+  .todos-list-txt {
+    margin-bottom: 20px;
     text-decoration: underline;
-    margin-left: -400px;
-    align-items: center;
     font-size: 30px;
     font-weight: bold;
     color: #57595d;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: sans-serif;
   }
-`;
-const Todos = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 510px;
-  margin-bottom: 5px;
-  vertical-align: baseline;
-
-  /* full written todo */
-  div {
-    display: inline-flex;
-  }
-  .box {
+  
+  .individualTodo-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 5fr 5fr 2fr;
+    grid-template-rows: 1fr;
+    justify-items: center;
     width: 350px;
-    height: 30px;
-    margin-top: 5px;
+    height: 60px;
     border: 3px solid #40e0d0;
-    border-radius: 10px;
+    border-radius: 7px;
     background-color: #ffffff;
-    padding: 10px;
-  }
-`;
 
-const Delete = styled.div`
-  button {
-    display: inline-flex;
-    vertical-align: baseline;
-    text-align: center;
-    border-radius: 10px;
-    text-decoration: none;
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 7px;
-    padding-bottom: 10px;
-    background-color: #40e0d0;
-    color: #ffffff;
-    margin-right: 100px;
-    order: 1;
-  }
-`;
-
-const CheckboxInput = styled.div`
-  margin-top: 7px;
-  display: inline-flex;
-  vertical-align: baseline;
-  margin-left: 10px;
-  order: 3;
-`;
-
-const ActualTodo = styled.div`
-  div {
-    width: 210px;
-    margin-left: -75px;
-    margin-top: 4px;
-    display: inline-flex;
-    vertical-align: baseline;
-    order: 2;
-    font-size: 20px;
-    vertical-align: baseline;
-    color: #57595d;
-    font-weight: bold;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    .todo-checkbox {
+      grid-column: 1 / span 2;
+      justify-self: center;
+      align-self: center;
+    }
+    .written-todo {
+      grid-column: 3 / span 2;
+      align-self: center;
+      justify-self: center;
+      font-size: 20px;
+      color: #57595d;
+      margin-right: 10px;
+      font-weight: bold;
+      font-family: sans-serif;
+    }  
+    .delete-btn {
+      grid-column: 5 / span 1;
+      align-self: center;
+      justify-self: end;
+      margin-right: 5px;
+      border-radius: 5px;
+      font-size: bold;
+      text-decoration: none;
+      width: 30px;
+      height: 30px;
+      padding: 7px;
+      background-color: #40e0d0;
+      border: 2px solid #57595d;
+      color: #ffffff;
+    }
   }
 `;
 
@@ -134,14 +103,15 @@ export default function App() {
   }
 
   function handleSubmit(event) {
-    const newTodos = [draft, ...todos]; //Making a copy//...=it will take everything//
+    const newTodos = [draft, ...todos]; //Making a copy//...= will take everything within the todos object//
     setDraft({ id: null, name: '', completed: false });
     setTodos(newTodos);
     event.preventDefault();
   }
 
+  /* With this function we delete a todo from the list by its id */
   function deleteTodo(id) {
-    const newTodos = todos.filter(todo => todo.id !== id);
+    const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   }
 
@@ -158,30 +128,22 @@ export default function App() {
           />
           <button> Add item </button>
           <br></br>
-          <p>List of todos:</p>
-          <Todos>
-            {todos.map(todoInList => {
+          <p className="todos-list-txt">Todos list:</p>
+          <div className="todo-list">
+            {todos.map((individualTodo) => {
               //map gives you the possibility of changing the object from string to object for example.
               return (
-                <div className="box" key={todoInList.id}>
-                  <Delete>
-                    <button
-                      className="x"
-                      onClick={() => deleteTodo(todoInList.id)}
-                    >
-                      x
-                    </button>
-                  </Delete>
-                  <CheckboxInput>
-                    <input
-                      value={todoInList.completed}
+                <div className="individualTodo-container" key={individualTodo.id}>
+                      <input
+                      className="todo-checkbox"
+                      value={individualTodo.completed}
                       type="checkbox"
                       onChange={() => {
                         setTodos(
-                          todos.map(todo => {
+                          todos.map((todo) => {
                             const newTodo = { ...todo };
-                            if (todo.id === todoInList.id) {
-                              //=== : mean equal // !== : the opposite
+                            if (todo.id === individualTodo.id) {
+                              //===  means equal // !== means the opposite
                               if (todo.completed === true) {
                                 newTodo.completed = false;
                               } else {
@@ -193,14 +155,17 @@ export default function App() {
                         );
                       }}
                     />
-                  </CheckboxInput>
-                  <ActualTodo>
-                    <div className="todoInListName">{todoInList.name}</div>
-                  </ActualTodo>
+                    <p className="written-todo">{individualTodo.name}</p>
+                  <button
+                      className="delete-btn"
+                      onClick={() => deleteTodo(individualTodo.id)}
+                    >
+                      x
+                  </button>  
                 </div>
               );
             })}
-          </Todos>
+          </div>
         </form>
       </Container>
     </>
